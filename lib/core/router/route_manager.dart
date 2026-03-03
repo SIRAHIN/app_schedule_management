6ed84379a1ca1a30b5b7850a8d47b76fe06f7ead
SIRAHIN <sirahin30@gmail.com>
2026-03-04 00:13:54 +0600

    New : Route created

---
+++ lib/core/router/route_manager.dart
@@ -0,0 +1,30 @@
+import 'package:app_schedule_management/presentation/screens/bottom_nav_screen/main_bottom_nav_screen.dart';
+import 'package:app_schedule_management/presentation/screens/splash_screen/splash_screen.dart';
+import 'package:go_router/go_router.dart';
+
+
+class RouteManager {
+  static final GoRouter router = GoRouter(
+    initialLocation: splashViewPath,
+    routes: [
+      GoRoute(
+        name: splashViewName,
+        path: splashViewPath,
+        builder: (context, state) => const SplashScreen(),
+      ),
+      GoRoute(
+        name: mainNavViewName,
+        path: mainNavViewPath,
+        builder: (context, state) => const MainBottomNavScreen(),
+      ),
+    ],
+  );
+}
+
+// Routes Paths 
+const String splashViewPath = '/';
+const String mainNavViewPath = '/mainNavView';
+
+// Routes Names 
+const String splashViewName = 'splashView';
+const String mainNavViewName = 'mainNavView';
