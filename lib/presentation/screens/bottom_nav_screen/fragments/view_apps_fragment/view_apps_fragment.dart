400acacb7cff55f6bd62cc9891128979276af873
SIRAHIN <sirahin30@gmail.com>
2026-03-04 00:09:33 +0600

    New : Project Structure, and View Screen Init

---
+++ lib/presentation/screens/bottom_nav_screen/fragments/view_apps_fragment/view_apps_fragment.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ViewAppsFragment extends StatelessWidget {
+  const ViewAppsFragment({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/presentation/screens/bottom_nav_screen/fragments/view_schedule_apps_fragment/view_schedule_apps_fragment.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ViewScheduleAppsFragment extends StatelessWidget {
+  const ViewScheduleAppsFragment({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
